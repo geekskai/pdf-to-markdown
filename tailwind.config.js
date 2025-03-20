358b1f251b4dfc174b018f594e6d03afbaabb87f
GanKai <gankai@channelwill.cn>
2025-03-20 20:45:02 +0800

    build(tailwind): disable preflight to avoid CSS conflicts
    The `preflight` plugin in Tailwind CSS resets default browser styles, which can cause conflicts with existing styles. Disabling it ensures compatibility with the current project's CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
   darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       fontSize: {
